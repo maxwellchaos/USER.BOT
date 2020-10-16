a613f58000d0d5babce33bb50cb6584538a71b01
BarzometrK <72938018+BarzometrK@users.noreply.github.com>
2020-10-16 18:24:06 +0300

    Добавил в общий проект

---
+++ USER.BOT/USER.BOT/AnswerForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace USER.BOT
+{
+    public partial class AnswerForm : Form
+    {
+        public AnswerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
