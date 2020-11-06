074b6249f3e3dc71dac1644abec0df77c9c1b5a2
Dynchikkk <72872351+Dynchikkk@users.noreply.github.com>
2020-11-06 18:38:41 +0300

    Лайкание постов при их количестве больше 100

--- USER.BOT/USER.BOT/LikerForm.Designer.cs
+++ USER.BOT/USER.BOT/LikerForm.Designer.cs
@@ -31,6 +31,8 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -58,11 +60,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Количестов ошибок:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
             // LikerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
@@ -78,5 +100,7 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- USER.BOT/USER.BOT/LikerForm.cs
+++ USER.BOT/USER.BOT/LikerForm.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 using System.Threading;
+using Microsoft.Win32;
 
 namespace USER.BOT
 {
@@ -18,6 +19,10 @@ namespace USER.BOT
         public string access_token;
         public string users_id;
         string UserId;
+
+        int bugs = 0;
+        int likes = 0;
+        int PostCount = 100;
 
         public LikerForm()
         {
@@ -31,15 +36,6 @@ namespace USER.BOT
             UserId = param[param.Length - 1];
             textBox1.Text = UserId;
 
-
-            //Запрос на получение информации о стене
-            string Request = "https://api.vk.com/method/wall.get?count&" + access_token + "&v=5.124";
-            WebClient client = new WebClient();
-            string Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
-
-            Wallget wg = JsonConvert.DeserializeObject<Wallget>(Answer);
-
-
             //Запрос на получение id
             string Request2 = "https://api.vk.com/method/utils.resolveScreenName?screen_name=" + UserId + "&" + access_token + "&v=5.124";
             WebClient client2 = new WebClient();
@@ -47,26 +43,62 @@ namespace USER.BOT
 
             idGet ig = JsonConvert.DeserializeObject<idGet>(Answer2);
 
-            //Прогресс на прогрессбар
-            int postCount = 0;
-            progressBar1.Minimum = postCount;
-            progressBar1.Maximum = wg.response.items.Count;
+            //Запрос на получение информации о стене
+            string Request = "https://api.vk.com/method/wall.get?count=100&" + "owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+            WebClient client = new WebClient();
+            string Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
 
-            //Массовый лайкинг
-            foreach (Wallget.Item item in wg.response.items)
+            Wallget wg = JsonConvert.DeserializeObject<Wallget>(Answer);
+            int Offset = 0;
+
+            Offset += wg.response.items.Count;
+
+            while (Offset < wg.response.count)
             {
-                postCount = postCount + 1;
-                progressBar1.Value = postCount;
+                //Повторный запрос на получение информации о стене 
+                Request = "https://api.vk.com/method/wall.get?count=100&Offset=" + Offset.ToString() + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                client = new WebClient();
+                Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
 
-                for (int i = 0; i < 10; i++)
+                wg = JsonConvert.DeserializeObject<Wallget>(Answer);
+                Offset += wg.response.items.Count;
+
+
+                //Прогресс на прогрессбар
+                int postCount = 0;
+                progressBar1.Minimum = postCount;
+                progressBar1.Maximum = wg.response.items.Count;
+
+                //Массовый лайкинг
+                foreach (Wallget.Item item in wg.response.items)
                 {
-                    Application.DoEvents();
-                    Thread.Sleep(50);
+                    postCount = postCount + 1;
+                    progressBar1.Value = postCount;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Application.DoEvents();
+                        Thread.Sleep(100);
+                    }
+
+                    string Request1 = "https://api.vk.com/method/likes.add?type=post&item_id=" + item.id + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                    WebClient client1 = new WebClient();
+                    string Answer1 = Encoding.UTF8.GetString(client1.DownloadData(Request1));
+
+                    if (Answer1.Contains("rror"))
+                    {
+                        bugs = bugs + 1;
+                    }
+                    else
+                    {
+                        likes++;
+                    }
+
+                    label2.Text = bugs.ToString() + "/" + likes.ToString() + "/" + wg.response.count.ToString();
                 }
 
-                string Request1 = "https://api.vk.com/method/likes.add?type=post&item_id=" + item.id + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
-                WebClient client1 = new WebClient();
-                string Answer1 = Encoding.UTF8.GetString(client1.DownloadData(Request1));
+
+
             }
         }
     }
