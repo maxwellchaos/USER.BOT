173aada2804eff8a77c319383c6396d7f6820cd3
Dynchikkk <72872351+Dynchikkk@users.noreply.github.com>
2020-11-13 18:39:45 +0300

    Проверка на капчу

--- USER.BOT/USER.BOT/LikerForm.Designer.cs
+++ USER.BOT/USER.BOT/LikerForm.Designer.cs
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LikerForm));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(16, 505);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Location = new System.Drawing.Point(12, 410);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1035, 34);
+            this.progressBar1.Size = new System.Drawing.Size(776, 28);
             this.progressBar1.TabIndex = 5;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 112);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(12, 91);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 22);
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "Вставьте ссылку или id";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(289, 90);
+            this.button1.Size = new System.Drawing.Size(217, 73);
             this.button1.TabIndex = 6;
             this.button1.Text = "Атака лайками";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,58 +68,70 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(313, 15);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(235, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(360, 17);
+            this.label1.Size = new System.Drawing.Size(282, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Количестов ошибок/постов пролайкано/всего постов\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(697, 15);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(523, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "0/0/0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1333, 1231);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 368);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(702, 34);
+            this.label3.Size = new System.Drawing.Size(550, 39);
             this.label3.TabIndex = 10;
-            this.label3.Text = "К сожалению, если на странице много постов, то это процесс займет несколько минут" +
-    ", ну или больше)\r\nПрограмма завершит процесс автоматически, поэтому вы можете сп" +
-    "окойно заниматься своими делами.\r\n";
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 117);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(217, 181);
+            this.webBrowser1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 304);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "Введите капчу если потребуется";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 186);
+            this.button2.Location = new System.Drawing.Point(12, 331);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Отправьте капчу";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // LikerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LikerForm";
             this.Text = "LikerForm";
             this.ResumeLayout(false);
@@ -135,6 +147,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
     }
 }
--- USER.BOT/USER.BOT/LikerForm.cs
+++ USER.BOT/USER.BOT/LikerForm.cs
@@ -21,6 +21,8 @@ namespace USER.BOT
         string UserId;
         public string captcha_get;
 
+        bool ButtonIsPressed = true;
+
         int bugs = 0;
         int likes = 0;
 
@@ -31,82 +33,117 @@ namespace USER.BOT
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            //Выделение id
-            string[] param = textBox1.Text.Split(new[] { "//", "/" }, StringSplitOptions.RemoveEmptyEntries);
-            UserId = param[param.Length - 1];
-            textBox1.Text = UserId;
-
-            //Запрос на получение id
-            string Request2 = "https://api.vk.com/method/utils.resolveScreenName?screen_name=" + UserId + "&" + access_token + "&v=5.124";
-            WebClient client2 = new WebClient();
-            string Answer2 = Encoding.UTF8.GetString(client2.DownloadData(Request2));
-
-            idGet ig = JsonConvert.DeserializeObject<idGet>(Answer2);
-
-            //Запрос на получение информации о стене
-            string Request = "https://api.vk.com/method/wall.get?count=100&" + "owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
-            WebClient client = new WebClient();
-            string Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
-
-            Wallget wg = JsonConvert.DeserializeObject<Wallget>(Answer);
-            int Offset = 0;
-
-            Offset += wg.response.items.Count;
-
-            int postCount = 0;
-            progressBar1.Minimum = 0;
-
-            while (Offset <= wg.response.count)
+            if (textBox1.Text == "")
             {
-                //Повторный запрос на получение информации о стене 
-                Request = "https://api.vk.com/method/wall.get?count=100&Offset=" + Offset.ToString() + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
-                client = new WebClient();
-                Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
 
-                wg = JsonConvert.DeserializeObject<Wallget>(Answer);
+            }
+            if (textBox1.Text == "Вставьте ссылку или id")
+            {
+
+            }
+            else
+            {
+                //Выделение id
+                string[] param = textBox1.Text.Split(new[] { "//", "/" }, StringSplitOptions.RemoveEmptyEntries);
+                UserId = param[param.Length - 1];
+                textBox1.Text = UserId;
+
+                //Запрос на получение id
+                string Request2 = "https://api.vk.com/method/utils.resolveScreenName?screen_name=" + UserId + "&" + access_token + "&v=5.124";
+                WebClient client2 = new WebClient();
+                string Answer2 = Encoding.UTF8.GetString(client2.DownloadData(Request2));
+
+                idGet ig = JsonConvert.DeserializeObject<idGet>(Answer2);
+
+                //Запрос на получение информации о стене
+                string Request = "https://api.vk.com/method/wall.get?count=100&" + "owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                WebClient client = new WebClient();
+                string Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
+
+                Wallget wg = JsonConvert.DeserializeObject<Wallget>(Answer);
+                int Offset = 0;
+
                 Offset += wg.response.items.Count;
 
+                //Прогресс на прогрессбар*
+                int postCount = 0;
+                progressBar1.Minimum = 0;
 
-                //Прогресс на прогрессбар
-                progressBar1.Maximum = wg.response.count;
-
-
-                //Массовый лайкинг
-                foreach (Wallget.Item item in wg.response.items)
+                while (Offset <= wg.response.count)
                 {
-                    postCount = postCount + 1;
-                    progressBar1.Value = postCount;
+                    //Повторный запрос на получение информации о стене 
+                    Request = "https://api.vk.com/method/wall.get?count=100&Offset=" + Offset.ToString() + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                    client = new WebClient();
+                    Answer = Encoding.UTF8.GetString(client.DownloadData(Request));
 
-                    for (int i = 0; i < 10; i++)
+                    wg = JsonConvert.DeserializeObject<Wallget>(Answer);
+                    Offset += wg.response.items.Count;
+
+                    //*
+                    progressBar1.Maximum = wg.response.count;
+
+                    //Массовый лайкинг
+                    foreach (Wallget.Item item in wg.response.items)
                     {
-                        Application.DoEvents();
-                        Thread.Sleep(100);
+                        //*
+                        postCount = postCount + 1;
+                        progressBar1.Value = postCount;
+
+                        //Cooldown
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Application.DoEvents();
+                            Thread.Sleep(100);
+                        }
+
+                        string Request1 = "https://api.vk.com/method/likes.add?type=post&item_id=" + item.id + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                        WebClient client1 = new WebClient();
+                        string Answer1 = Encoding.UTF8.GetString(client1.DownloadData(Request1));
+
+                        //Получение и отправка капчи
+                        if (Answer1.Contains("rror"))
+                        {
+
+                            if (Answer1.Contains("aptcha"))
+                            {
+                                ButtonIsPressed = false;
+
+                                string Answer4 = "{ \"error\":{ \"error_code\":14,\"error_msg\":\"Captcha needed\",\"request_params\":[{ \"key\":\"type\",\"value\":\"post\"},{ \"key\":\"item_id\",\"value\":\"185\"},{ \"key\":\"owner_id\",\"value\":\"422303825\"},{ \"key\":\"v\",\"value\":\"5.124\"},{ \"key\":\"method\",\"value\":\"likes.add\"},{ \"key\":\"oauth\",\"value\":\"1\"}],\"captcha_sid\":\"337894471349\",\"captcha_img\":\"https://api.vk.com/captcha.php?sid=337894471349&s=1\"}}";
+                                CaptchaGet cg = JsonConvert.DeserializeObject<CaptchaGet>(Answer4);
+                                webBrowser1.Navigate(cg.error.captcha_img);
+
+                                string Request5 = "https://api.vk.com/method/likes.add?type=post&item_id=" + item.id + "&captcha_sid" + cg.error.captcha_sid + "&captcha_key" + textBox2.Text + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
+                                WebClient client5 = new WebClient();
+                                string Answer5 = Encoding.UTF8.GetString(client5.DownloadData(Request5));
+
+                                while (ButtonIsPressed == false)
+                                {
+                                    Application.DoEvents();
+                                    Thread.Sleep(10);
+                                }
+                            }
+                            else
+                            {
+                                bugs = bugs + 1;
+                            }
+                        }
+                        else
+                        {
+                            likes++;
+                        }
+
+                        label2.Text = bugs.ToString() + "/" + likes.ToString() + "/" + wg.response.count.ToString();
+
                     }
-
-                    string Request1 = "https://api.vk.com/method/likes.add?type=post&item_id=" + item.id + "&owner_id=" + ig.response.object_id + "&" + access_token + "&v=5.124";
-                    WebClient client1 = new WebClient();
-                    string Answer1 = Encoding.UTF8.GetString(client1.DownloadData(Request1));
-
-                    if (Answer1.Contains("rror"))
-                    {
-                        bugs = bugs + 1;
-                    }
-                    else
-                    {
-                        likes++;
-                    }
-
-                    label2.Text = bugs.ToString() + "/" + likes.ToString() + "/" + wg.response.count.ToString();
-
                 }
+
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
-        {
-            //string Answer = { "error":{ "error_code":14,"error_msg":"Captcha needed","request_params":[{ "key":"type","value":"post"},{ "key":"item_id","value":"185"},{ "key":"owner_id","value":"422303825"},{ "key":"v","value":"5.124"},{ "key":"method","value":"likes.add"},{ "key":"oauth","value":"1"}],"captcha_sid":"337894471349","captcha_img":"https:\/\/api.vk.com\/captcha.php?sid=337894471349&s=1"}
-            //string[] taram = Answer.Split(new[] { "//", "/" }, StringSplitOptions.RemoveEmptyEntries);
+        {            
+            ButtonIsPressed = true;
         }
-
     }
+
 }
