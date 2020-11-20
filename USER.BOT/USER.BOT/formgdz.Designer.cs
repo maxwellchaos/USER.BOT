8e72d6e108ff4ea096efb3d18b2b301e2cd70534
Scoup-55 <72984830+Scoup-55@users.noreply.github.com>
2020-11-20 18:35:14 +0300

    Add files via upload
    получение ответа его считывание и отправка документа по его запросу

--- USER.BOT/USER.BOT/formgdz.Designer.cs
+++ USER.BOT/USER.BOT/formgdz.Designer.cs
@@ -29,30 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.labelgrope = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // labelgrope
-            // 
-            this.labelgrope.AutoSize = true;
-            this.labelgrope.Location = new System.Drawing.Point(184, 533);
-            this.labelgrope.Name = "labelgrope";
-            this.labelgrope.Size = new System.Drawing.Size(51, 20);
-            this.labelgrope.TabIndex = 0;
-            this.labelgrope.Text = "label1";
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "П.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1021, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Б.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1025, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
             // 
             // formgdz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1578, 807);
-            this.Controls.Add(this.labelgrope);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "formgdz";
             this.Text = "formgdz";
             this.Load += new System.EventHandler(this.formgdz_Load);
@@ -62,8 +96,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelgrope;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- USER.BOT/USER.BOT/formgdz.cs
+++ USER.BOT/USER.BOT/formgdz.cs
@@ -15,7 +15,7 @@ namespace USER.BOT
     {
         public string access_token;
         public string user_id;
-
+        string GroupAccess_token = "access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3";
 
         public formgdz()
         {
@@ -50,88 +50,110 @@ namespace USER.BOT
 
         private void formgdz_Load(object sender, EventArgs e)
         {
-          
-            
-             
+
+
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-           
-           
-            
-            //00(предмет)_00(класс)_00(автор)_0000(упражнение)
+           //00(предмет)_00(класс)_00(автор)_0000(упражнение)
             Random rnd = new Random();
+            int random_id2 = rnd.Next();
             string reqeuest2 = "https://api.vk.com/method/messages.getConversations?access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
             WebClient cl = new WebClient();
             string answer2 = Encoding.UTF8.GetString(cl.DownloadData(reqeuest2));
-            reqeuest2 = "https://api.vk.com/method/docs.get?owner_id=-199265164&type=4&access_token=7a695e3b21b5632316f71ca64ea5680d3bb3083eff4cbab89c826c6b44363b39640c974c3712a1228733e&v=5.124";
+            messagesgetConversations mg = JsonConvert.DeserializeObject<messagesgetConversations>(answer2);
+            reqeuest2 = "https://api.vk.com/method/docs.get?owner_id=-199265164&type=4&" + access_token + "&v=5.124";
             answer2 = Encoding.UTF8.GetString(cl.DownloadData(reqeuest2));
             docsget dc = JsonConvert.DeserializeObject<docsget>(answer2);
-            
-            if(dc.response.items[0].title.Contains("00_00_00_0000_"))
-            {
-                //нашли нужный файл
-                //отправить его человеку
+
+
+   
+
+
+            foreach (docsget.Item item1 in dc.response.items)
+            { 
+                    if (item1.title.Contains(mg.response.items[0].last_message.text.ToString()))
+                    {
+                        string doc_id = "doc" + item1.owner_id.ToString() + "_" + item1.id.ToString();
+                        // item1.url
+                        // random_id2 = rnd.Next();
+                        reqeuest2 = "https://api.vk.com/method/messages.send?attachment=" + doc_id + "&message=держии ответ&type=4&user_id=" +mg.response.items[0].last_message.from_id.ToString()+ "&random_id=" + random_id2 + "&"+ GroupAccess_token + "&v=5.124";
+                        answer2 = Encoding.UTF8.GetString(cl.DownloadData(reqeuest2));
+
+                    }
             }
-            
-            
-            messagesgetConversations mgc = JsonConvert.DeserializeObject<messagesgetConversations>(answer2);
-            labelgrope.Text = mgc.response.items[0].last_message.text;
-            
-            
+
+            string massegeid = mg.response.items[0].last_message.id.ToString();
+            massegeid = massegeid + "," + "-1";
+            reqeuest2 = "https://api.vk.com/method/messages.getById?message_ids=" + massegeid.ToString() + "&group_id=199265164&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+
+            answer2 = Encoding.UTF8.GetString(cl.DownloadData(reqeuest2));
+            messagesgetById mgbi = JsonConvert.DeserializeObject<messagesgetById>(answer2);
+           
+           
+
+
+            //messagesgetConversations mgc = JsonConvert.DeserializeObject<messagesgetConversations>(answer2);
+            //labelgrope.Text = mgc.response.items[0].last_message.text;
             reqeuest2 = "https://api.vk.com/method/messages.getConversations?filter=unanswered&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-            WebClient wc2 = new WebClient();
-            string answer4 = Encoding.UTF8.GetString(wc2.DownloadData(reqeuest2));
-            messagesgetConversations mgc1 = JsonConvert.DeserializeObject<messagesgetConversations>(answer4);
-            foreach (messagesgetConversations.Item item in mgc1.response.items)
-            {
-                string[] separator = new string[2];
-                separator[0] = "_";
-                separator[1] = "_";
-                string[] param = item.last_message.text.Split(separator, StringSplitOptions.None);
-                
+                WebClient wc2 = new WebClient();
+                string answer4 = Encoding.UTF8.GetString(wc2.DownloadData(reqeuest2));
+                messagesgetConversations mgc1 = JsonConvert.DeserializeObject<messagesgetConversations>(answer4);
+                foreach (messagesgetConversations.Item item in mgc1.response.items)
+                {
+                    string[] separator = new string[2];
+                    separator[0] = "_";
+                    separator[1] = "_";
+                    string[] param = item.last_message.text.Split(separator, StringSplitOptions.None);
 
-                WebClient wc = new WebClient();
-                int random_id = rnd.Next();
-                // messagesSend ms = JsonConvert.DeserializeObject<messagesSend>(answer3);
-                if (item.last_message.text == "1")
-                {
-                    random_id = rnd.Next();
-                    reqeuest2 = "https://api.vk.com/method/messages.send?message=https://gdz.ru/class-6/matematika/a-g-merzlyak/&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-                    string answer6 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
-                }
-                if (item.last_message.text == "2")
-                {
-                    random_id = rnd.Next();
-                    reqeuest2 = "https://api.vk.com/method/messages.send?attachment=doc-199265164_571585875&message=держии ответ/&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-                    string answer6 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
-                }
-                if (item.last_message.text == "старт")
-                {
-                    reqeuest2 = "https://api.vk.com/method/messages.send?message=Я знаю зачем ты пришёл, я всё устрою ты главное следуй инструкциям которые я тебе дам пример:00(предмет)_00(класс)_00(автор)_0000(упражнение)&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-                    random_id = rnd.Next();
-                    string answer3 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
-                }
-                if (item.last_message.text == "Старт")
-                {
-                    reqeuest2 = "https://api.vk.com/method/messages.send?message=Я знаю зачем ты пришёл, я всё устрою ты главное следуй инструкциям которые я тебе дам пример: 00(предмет)_00(класс)_00(автор)_0000(упражнение)&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-                    random_id = rnd.Next();
-                    string answer8 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
-                }
-                if (item.last_message.text != "1" && item.last_message.text != "2" && item.last_message.text != "старт" && item.last_message.text != "Старт")
-                {
-                    random_id = rnd.Next();
-                    reqeuest2 = "https://api.vk.com/method/messages.send?message=непонял вопроса&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
-                    string answer7 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
-                    random_id = rnd.Next();
 
-                    reqeuest2 = "https://api.vk.com/method/messages.send?message=Выбири предмет 1=математика 2=русский&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                    WebClient wc = new WebClient();
+                    int random_id = rnd.Next();
+                    // messagesSend ms = JsonConvert.DeserializeObject<messagesSend>(answer3);
+                    if (item.last_message.text == "1")
+                    {
+                        random_id = rnd.Next();
+                        reqeuest2 = "https://api.vk.com/method/messages.send?message=https://gdz.ru/class-6/matematika/a-g-merzlyak/&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                        string answer6 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                    }
+                    if (item.last_message.text == "2")
+                    {
+                        random_id = rnd.Next();
+                        reqeuest2 = "https://api.vk.com/method/messages.send?attachment=doc-199265164_571585875&message=держии ответ&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                        string answer6 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                    }
+                    if (item.last_message.text == "старт")
+                    {
+                        reqeuest2 = "https://api.vk.com/method/messages.send?message=Я знаю зачем ты пришёл, я всё устрою ты главное следуй инструкциям которые я тебе дам пример:00(предмет)_00(класс)_00(автор)_0000(упражнение)&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                        random_id = rnd.Next();
+                        string answer3 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                    }
+                    if (item.last_message.text == "Старт")
+                    {
+                        reqeuest2 = "https://api.vk.com/method/messages.send?message=Я знаю зачем ты пришёл, я всё устрою ты главное следуй инструкциям которые я тебе дам пример: 00(предмет)_00(класс)_00(автор)_0000(упражнение)&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                        random_id = rnd.Next();
+                        string answer8 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                    }
+                    if (item.last_message.text != "1" && item.last_message.text != "2" && item.last_message.text != "старт" && item.last_message.text != "Старт")
+                    {
+                        random_id = rnd.Next();
+                        reqeuest2 = "https://api.vk.com/method/messages.send?message=непонял вопроса&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+                        string answer7 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                        random_id = rnd.Next();
 
-                    string answer5 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                        reqeuest2 = "https://api.vk.com/method/messages.send?message=Выбири предмет 1=математика 2=русский&user_id=" + item.last_message.from_id.ToString() + "&random_id=" + random_id + "&access_token=e2da676d069c28cfce6428a770c3e3413f85260468038237ff5a07c2a57975602a0bd8828786c116d27b3&v=5.124";
+
+                        string answer5 = Encoding.UTF8.GetString(wc.DownloadData(reqeuest2));
+                    }
+
                 }
-
             }
+
+        private void labelgrope_Click(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    } 
