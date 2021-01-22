47ffcfc26a22156d41cebde0354c783c88122894
VatolinE <72877947+VatolinE@users.noreply.github.com>
2021-01-22 18:30:05 +0300

    Исправление

---
+++ USER.BOT/USER.BOT/FormChatBot.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace USER.BOT
+{
+    partial class FormChatBot
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ButtonChatBot = new System.Windows.Forms.Button();
+            this.City1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Name1 = new System.Windows.Forms.Label();
+            this.Name2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.Name3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonChatBot
+            // 
+            this.ButtonChatBot.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ButtonChatBot.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ButtonChatBot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonChatBot.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ButtonChatBot.Location = new System.Drawing.Point(830, 376);
+            this.ButtonChatBot.Name = "ButtonChatBot";
+            this.ButtonChatBot.Size = new System.Drawing.Size(215, 112);
+            this.ButtonChatBot.TabIndex = 0;
+            this.ButtonChatBot.Text = "Начать";
+            this.ButtonChatBot.UseVisualStyleBackColor = false;
+            this.ButtonChatBot.Visible = false;
+            this.ButtonChatBot.Click += new System.EventHandler(this.ButtonChatBot_Click);
+            // 
+            // City1
+            // 
+            this.City1.AutoSize = true;
+            this.City1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.City1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.City1.Location = new System.Drawing.Point(12, 401);
+            this.City1.Name = "City1";
+            this.City1.Size = new System.Drawing.Size(49, 25);
+            this.City1.TabIndex = 1;
+            this.City1.Text = "City";
+            this.City1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(12, 463);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // Name1
+            // 
+            this.Name1.AutoSize = true;
+            this.Name1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name1.Location = new System.Drawing.Point(14, 376);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(40, 13);
+            this.Name1.TabIndex = 3;
+            this.Name1.Text = "Город:";
+            this.Name1.Visible = false;
+            // 
+            // Name2
+            // 
+            this.Name2.AutoSize = true;
+            this.Name2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name2.Location = new System.Drawing.Point(14, 441);
+            this.Name2.Name = "Name2";
+            this.Name2.Size = new System.Drawing.Size(99, 13);
+            this.Name2.TabIndex = 4;
+            this.Name2.Text = "Последняя Буква:";
+            this.Name2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(978, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Нажми!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(158, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Name3
+            // 
+            this.Name3.AutoSize = true;
+            this.Name3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name3.Location = new System.Drawing.Point(160, 441);
+            this.Name3.Name = "Name3";
+            this.Name3.Size = new System.Drawing.Size(123, 13);
+            this.Name3.TabIndex = 7;
+            this.Name3.Text = "Предпоследняя Буква:";
+            this.Name3.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Бот будет работать пока работает программа!!!";
+            // 
+            // FormChatBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1057, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Name3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Name2);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.City1);
+            this.Controls.Add(this.ButtonChatBot);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "FormChatBot";
+            this.Text = "FormChatBot";
+            this.Load += new System.EventHandler(this.FormChatBot_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonChatBot;
+        private System.Windows.Forms.Label City1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Name1;
+        private System.Windows.Forms.Label Name2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Name3;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
---
+++ USER.BOT/USER.BOT/FormChatBot.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace USER.BOT
+{
+    public partial class FormChatBot : Form
+    {
+        public string access_token;
+        public string GroupAccess_token = "access_token=09c5ce6b65692dc6fdb2684201998d347d52d2dcae7dd03dc052e16f3c08c097a6803427496de41502a8e";
+        public string user_id;
+        public string messages;
+
+        public FormChatBot()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonChatBot_Click(object sender, EventArgs e)
+        {
+            //возвращает запрос на сообщения
+            string Request = "https://api.vk.com/method/messages.getConversations?fields=bdate&" +
+            GroupAccess_token + "&v=5.124";
+
+            WebClient cl = new WebClient();
+            //показывает последнее сообщение
+            string Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+            messagesGetConversations mgc = JsonConvert.DeserializeObject<messagesGetConversations>(Answer);
+            City1.Text = mgc.response.items[0].last_message.text;
+            string lastMessage = mgc.response.items[0].last_message.text;
+            int len = lastMessage.Length;
+            string lastLetter = lastMessage.Remove(0, len - 1);
+            label2.Text = lastLetter;
+            Random rnd = new Random();
+            int result = rnd.Next(10000000);
+            string PredLetter = lastMessage.Remove(0, len - 2);
+            label3.Text = PredLetter;
+
+            City1.Visible = true;
+            Name1.Visible = true;
+            Name2.Visible = true;
+            label2.Visible = true;
+
+            //порт к папке с городами
+            Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+            string str = Application.StartupPath + @"\Документ.txt";
+            string[] txtFile = System.IO.File.ReadAllLines(@"C:\Users\Виктор\Desktop\Документ.txt.txt", Encoding.Default);
+            foreach (string City in txtFile)
+            {
+
+                Application.DoEvents();
+                if (lastLetter.ToUpper() == City[0].ToString())
+                {
+                    Request = "https://api.vk.com/method/messages.send?message=" + City + "&random_id=" + result.ToString() + "&peer_id=471929958&" +
+                    GroupAccess_token + "&v=5.124";
+                    WebClient cs = new WebClient();
+                    Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+                }
+
+            }
+            Application.DoEvents();
+            if (PredLetter.ToUpper() == label3.ToString())
+            {
+                Request = "https://api.vk.com/method/messages.send?message=" + label3 + "&random_id=" + result.ToString() + "&peer_id=471929958&" +
+                GroupAccess_token + "&v=5.124";
+                WebClient cs = new WebClient();
+                Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonChatBot.Visible = true;
+            button1.Visible = false;
+        }
+
+        public string GetAnswer(string messages, string access_token)
+        {
+            string mes = messages + access_token + "&v=5.124";
+            WebClient cl = new WebClient();
+            string GetAnswer = Encoding.UTF8.GetString(cl.DownloadData(messages));
+            return GetAnswer;
+        }
+
+        private void FormChatBot_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //возвращает запрос на сообщения
+            string Request = "https://api.vk.com/method/messages.getConversations?filter=unread&fields=bdate&" +
+            GroupAccess_token + "&v=5.124";
+
+            WebClient cl = new WebClient();
+            //показывает последнее сообщение
+            string Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+            messagesGetConversations mgc = JsonConvert.DeserializeObject<messagesGetConversations>(Answer);
+            if (mgc.response.count == 0)
+            {
+                return;
+            }
+            City1.Text = mgc.response.items[0].last_message.text;
+            string lastMessage = mgc.response.items[0].last_message.text;
+            int len = lastMessage.Length;
+            string lastLetter = lastMessage.Remove(0, len - 1);
+            label2.Text = lastLetter;
+            Random rnp = new Random();
+            int result = rnp.Next(10000000);
+            string PredLetter = lastMessage.Remove(0, len - 2);
+            PredLetter = PredLetter.Remove(1, 1);
+            label3.Text = PredLetter;
+
+            City1.Visible = true;
+            Name1.Visible = true;
+            Name2.Visible = true;
+            Name3.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+
+            //порт к папке с городами
+            Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+            string str = Application.StartupPath + @"\Документ.txt";
+            string[] txtFile = System.IO.File.ReadAllLines(@"C:\Users\Виктор\Desktop\Документ.txt.txt", Encoding.Default);
+            Random rnd = new Random();
+            foreach (string City in txtFile)
+            {
+
+                Application.DoEvents();
+                int Number = rnd.Next(10);
+
+                if (lastLetter.ToUpper() == City[0].ToString())
+                    if (Number < 5)
+                    {
+                        Request = "https://api.vk.com/method/messages.send?message=" + City + "&random_id=" + result.ToString() + "&peer_id=471929958&" +
+                        GroupAccess_token + "&v=5.124";
+                        WebClient cs = new WebClient();
+                        Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+                    }
+            }
+            foreach (string City in txtFile)
+            {
+
+                Application.DoEvents();
+                int Number = rnd.Next(10);
+
+                if (PredLetter.ToUpper() == City[0].ToString())
+                    if (Number < 5)
+                    {
+                        Request = "https://api.vk.com/method/messages.send?message=" + City + "&random_id=" + result.ToString() + "&peer_id=471929958&" +
+                        GroupAccess_token + "&v=5.124";
+                        WebClient cs = new WebClient();
+                        Answer = Encoding.UTF8.GetString(cl.DownloadData(Request));
+                    }
+            }
+        }
+    }
+}
