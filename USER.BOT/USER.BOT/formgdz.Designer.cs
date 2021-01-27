ed6a2da21f6738000cffd408f4d088800497e5b5
Scoup-55 <72984830+Scoup-55@users.noreply.github.com>
2021-01-27 18:11:58 +0300

    Работает на других аккаунтах

--- USER.BOT/USER.BOT/formgdz.Designer.cs
+++ USER.BOT/USER.BOT/formgdz.Designer.cs
@@ -41,6 +41,9 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textboxGroup = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // timer1
@@ -99,27 +102,27 @@
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(69, 202);
+            this.textBox1.Location = new System.Drawing.Point(1242, 724);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1191, 26);
+            this.textBox1.Size = new System.Drawing.Size(10, 26);
             this.textBox1.TabIndex = 4;
             this.textBox1.Visible = false;
             // 
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(69, 371);
+            this.textBox2.Location = new System.Drawing.Point(1126, 724);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(1183, 432);
+            this.textBox2.Size = new System.Drawing.Size(102, 68);
             this.textBox2.TabIndex = 6;
             this.textBox2.Visible = false;
             // 
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(1366, 637);
+            this.button1.Location = new System.Drawing.Point(1284, 724);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 66);
             this.button1.TabIndex = 7;
@@ -131,37 +134,67 @@
             // textBox3
             // 
             this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(69, 259);
+            this.textBox3.Location = new System.Drawing.Point(1033, 724);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(1191, 93);
+            this.textBox3.Size = new System.Drawing.Size(87, 59);
             this.textBox3.TabIndex = 8;
             this.textBox3.Visible = false;
             // 
             // textboxGroup
             // 
-            this.textboxGroup.Location = new System.Drawing.Point(69, 423);
+            this.textboxGroup.Location = new System.Drawing.Point(46, 400);
             this.textboxGroup.Multiline = true;
             this.textboxGroup.Name = "textboxGroup";
-            this.textboxGroup.Size = new System.Drawing.Size(1183, 139);
+            this.textboxGroup.Size = new System.Drawing.Size(1206, 162);
             this.textboxGroup.TabIndex = 9;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1313, 371);
+            this.button2.Location = new System.Drawing.Point(528, 224);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(186, 65);
             this.button2.TabIndex = 10;
-            this.button2.Text = "button2";
+            this.button2.Text = "Вкл";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ключь доступа";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 594);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ID группы";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(69, 641);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(645, 91);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // formgdz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textboxGroup);
             this.Controls.Add(this.textBox3);
@@ -174,6 +207,7 @@
             this.Controls.Add(this.textBox2);
             this.Name = "formgdz";
             this.Text = "formgdz";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formgdz_FormClosing);
             this.Load += new System.EventHandler(this.formgdz_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -193,5 +227,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textboxGroup;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
